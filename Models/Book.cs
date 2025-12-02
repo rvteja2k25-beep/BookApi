58d427a33d46ed6d04c9d3a0a3c63770e51dfe21
ravi <rvteja2k13@gmail.com>
2025-12-02 16:04:44 +0000

    updated author column

--- Models/Book.cs
+++ Models/Book.cs
@@ -4,5 +4,7 @@ public class Book
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
+
+    public String Author {get;set;} =String.Empty;
     public bool IsCheckedOut { get; set; }
 }
